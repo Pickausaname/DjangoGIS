7513dda9ac00657df769621afb1f45ba28b674fd
Nik Zem <utroranneesr@gmail.com>
2017-03-13 12:08:39 +0200

    create new branch, create method get.short.text

--- gis/models.py
+++ gis/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+SHORT_TEXT_LEN=700
+
 # Create your models here.
 class Articles(models.Model):
     title = models.CharField(max_length=200)
@@ -10,4 +12,10 @@ class Articles(models.Model):
     user = models.ForeignKey(User)
 
     def __str__(self):
-        return(self.title)
+        return self.title
+
+    def get_short_text(self):
+        if len(self.text)>SHORT_TEXT_LEN:
+            return self.text[:SHORT_TEXT_LEN]
+        else:
+            return self.text
